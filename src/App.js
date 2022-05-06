053e19a670282dafb7584f20e27df2561bb57d40
Konrad Trzciński <konrad.trzcinski@softwarehut.com>
2022-05-06 12:27:38 +0200

    Initial app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
 
 function App() {
+  const [toggled, setToggled] = React.useState(false);
+  const handleClick = () => setToggled(prev => !prev);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p onClick={handleClick}>
+      State is {toggled ? 'ON' : 'OFF'}
+    </p>
   );
 }
 
